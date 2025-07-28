b62f643d728bcc1133825871c07db1f960830e9e
jesuryes <161269641+jesuryes@users.noreply.github.com>
2025-07-28 10:01:22 -0600

    cambios

--- src/main/java/com/hotel/hotel/review/repository/PlaceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotel.hotel.review.repository;
-
-import com.hotel.hotel.review.entity.Place;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaceRepository extends JpaRepository<Place, Long> {
-}
